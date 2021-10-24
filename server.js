65eeeab45668a85d998d8c6389e08fdbff84fade
sundaramoorthi SNS <sundarbtechict@gmail.com>
2021-10-24 16:04:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./dist/leaderboardApp"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/leaderboardApp/" })
+  res.sendFile("index.html", { root: "dist/OrgoForm/" })
 );
 
 app.listen(process.env.PORT || 8080);
