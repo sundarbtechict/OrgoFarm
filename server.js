f4c56ad1fc2266631214b9e16b573be274555429
sundaramoorthi SNS <sundarbtechict@gmail.com>
2021-10-24 16:19:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/leaderboardApp"));
+app.use(express.static("./dist/OrgoForm"));
 
 app.get("/*", (req, res) =>
   res.sendFile("index.html", { root: "dist/OrgoForm/" })
